3c2020c33aa4789bf53255a94e9f1a31597e577e
Mirjana Cale <mirjana083@gmail.com>
2024-06-29 21:03:27 +0000

    Define classes and functions

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#imports
+import os
+from tkinter import *
+import random
+
+class Dragon:
+    pass
+
+class Gold:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
+
