46a61c2dd9999da341d3748f671617ebf082ac70
Mirjana Cale <mirjana083@gmail.com>
2024-07-08 12:50:11 +0000

    Refactor initUI method in game class,set geometry,window title

--- run.py
+++ run.py
@@ -23,17 +23,69 @@ class Gold:
     def __init__(self):
         self.coordinates = [random.randint(0, 790), random.randint(0, 490)]  
         # random initial coordinates of the gold
+        
+class DragonGame(QWidget):
+    def __init__(self):
+        super().__init__()
 
+        self.initUI()
+        
+    def initUI(self):
+        # Initialize the user interface
+        print("Initializing UI...")
 
+        # Set the geometry of the window
+        self.setGeometry(300, 300, 800, 500)
+        print("Geometry set.")
 
+        # Set the window title
+        self.setWindowTitle('Dragon Game')
+        print("Window title set.")
 
+        # Create a new dragon
+        self.dragon = Dragon()
+        print("Dragon created.")
 
+        # Create a new piece of gold
+        self.gold = Gold()
+        print("Gold created.")
 
+        # Initialize the score to 0
+        self.score = 0
+        print("Score initialized to 0.")
 
+        # Load the high score from file
+        self.high_score = self.load_high_score()
+        print("High score loaded.")
 
-def next_turn():
+        # Set the initial direction of the dragon to up
+        self.direction = "up"
+        print("Direction set to 'up'.")
+
+        # Show the window
+        self.show()
+        print("UI shown.")
+
+        # Print a message to indicate that the UI has been initialized
+        print("UI initialization complete.")
     
-    pass
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawGame(qp)
+        qp.end()    
+    
+        
+
+
+
+
+
+
+
+
+
+    
 
 def change_direction(new_direction):
     pass
